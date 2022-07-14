486591a74169ff31f313d7019f4cc4febca73686
Alessia18 <alessiaangelone18@gmail.com>
2022-07-14 23:19:05 +0200

    Google Calendar

--- containers/node/app.js
+++ containers/node/app.js
@@ -8,8 +8,6 @@ var logger = require('morgan');
 const dotenv = require("dotenv");
 var passport = require('passport');
 var session = require('express-session');
-//var FacebookStrategy = require('passport-facebook').Strategy;
-var  bodyParser        =     require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +15,105 @@ var newsRouter = require('./routes/news');
 
 var app = express();
 dotenv.config({ path: "./config/.env" });
+
+const { google } = require('googleapis');
+const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+const calendarId = process.env.CALENDAR_ID;
+
+const SCOPES = 'https://www.googleapis.com/auth/calendar';
+const calendar = google.calendar({version: "v3"});
+
+const auth = new google.auth.JWT(
+  CREDENTIALS.client_email,
+  null,
+  CREDENTIALS.private_key,
+  SCOPES
+)
+
+// Your TIMEOFFSET Offset
+const TIMEOFFSET = '+05:30';
+
+// Get date-time string for calender
+const dateTimeForCalander = () => {
+
+    let date = new Date();
+
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    let day = date.getDate();
+    if (day < 10) {
+        day = `0${day}`;
+    }
+    let hour = date.getHours();
+    if (hour < 10) {
+        hour = `0${hour}`;
+    }
+    let minute = date.getMinutes();
+    if (minute < 10) {
+        minute = `0${minute}`;
+    }
+
+    let newDateTime = `${year}-${month}-${day}T${hour}:${minute}:00.000${TIMEOFFSET}`;
+
+    let event = new Date(Date.parse(newDateTime));
+
+    let startDate = event;
+    // Delay in end time is 1
+    let endDate = new Date(new Date(startDate).setHours(startDate.getHours()+1));
+
+    return {
+        'start': startDate,
+        'end': endDate
+    }
+};
+
+// Insert new event to Google Calendar
+const insertEvent = async (event) => {
+
+    try {
+        let response = await calendar.events.insert({
+            auth: auth,
+            calendarId: calendarId,
+            resource: event
+        });
+    
+        if (response['status'] == 200 && response['statusText'] === 'OK') {
+            return 1;
+        } else {
+            return 0;
+        }
+    } catch (error) {
+        console.log(`Error at insertEvent --> ${error}`);
+        return 0;
+    }
+};
+
+let dateTime = dateTimeForCalander();
+
+// Event for Google Calendar
+let event = {
+    'summary': `This is the summary.`,
+    'description': `This is the description.`,
+    'start': {
+        'dateTime': dateTime['start'],
+        'timeZone': 'Asia/Kolkata'
+    },
+    'end': {
+        'dateTime': dateTime['end'],
+        'timeZone': 'Asia/Kolkata'
+    }
+};
+
+insertEvent(event)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -51,20 +148,7 @@ app.get('/google', passport.authenticate('google', { scope: ['profile', 'email']
 app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
   //res.redirect('/');
   res.cookie("username", req.user.displayName.toLowerCase().replaceAll(" ","."));
-  res.redirect('/news');
-})
-
-
-
-
-require('./authenticate2');
-
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['public_profile', 'email'] }));
-
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
-  //res.redirect('/');
-  res.cookie("username", req.user.displayName.toLowerCase().replaceAll(" ","."));
-  res.redirect('/news');
+  res.redirect('/users/news');
 })
 
 // catch 404 and forward to error handler
